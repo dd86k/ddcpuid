c597fccb0023c2a5b0c4ae67e0c84f8f5990be8b
dd86k <dd@dax.moe>
2022-03-27 09:26:09 -0400

    Bump v0.20.0

--- src/ddcpuid.d
+++ src/ddcpuid.d
@@ -52,7 +52,7 @@ version (DigitalMars) {
 	version = DMDLDC;	// DMD or LDC compilers
 } else static assert(0, "Unsupported compiler");
 
-enum DDCPUID_VERSION   = "0.20.0-rc.3";	/// Library version
+enum DDCPUID_VERSION   = "0.20.0";	/// Library version
 private enum CACHE_LEVELS = 6;	/// For buffer
 private enum CACHE_MAX_LEVEL = CACHE_LEVELS - 1;
 
